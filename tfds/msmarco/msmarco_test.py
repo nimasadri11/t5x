1b0aac65b4c3e770bfcc8d5a19451d9b3f0104f8
Nima Sadri <nimasadri11@gmail.com>
2022-03-16 23:47:33 -0400

    added tfds templates for msmarco

---
+++ tfds/msmarco/msmarco_test.py
@@ -0,0 +1,24 @@
+"""msmarco dataset."""
+
+import tensorflow_datasets as tfds
+from . import msmarco
+
+
+class MsmarcoTest(tfds.testing.DatasetBuilderTestCase):
+  """Tests for msmarco dataset."""
+  # TODO(msmarco):
+  DATASET_CLASS = msmarco.Msmarco
+  SPLITS = {
+      'train': 3,  # Number of fake train example
+      'test': 1,  # Number of fake test example
+  }
+
+  # If you are calling `download/download_and_extract` with a dict, like:
+  #   dl_manager.download({'some_key': 'http://a.org/out.txt', ...})
+  # then the tests needs to provide the fake output paths relative to the
+  # fake data directory
+  # DL_EXTRACT_RESULT = {'some_key': 'output_file1.txt', ...}
+
+
+if __name__ == '__main__':
+  tfds.testing.test_main()
